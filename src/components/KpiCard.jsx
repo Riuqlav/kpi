691a9ab2aa15f6cd45397b739caa57959e8833bb
Riuqlav <valquirsolza.vs@gmail.com>
2023-11-07 13:19:11 +0100

    WIP styling

--- src/components/KpiCard.jsx
+++ src/components/KpiCard.jsx
@@ -1,36 +1,83 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleInfo, faArrowUp, faArrowDown, faGripLines } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleInfo,
+  faArrowUp,
+  faArrowDown,
+  faGripLines,
+  faCoffee,
+  faAnchor,
+  faBicycle,
+  faBell,
+  faBolt,
+  faBomb,
+  faBook,
+  faCamera,
+  faCar,
+  faAppleAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
-const KpiCard = ({ name, tooltipText, value, trend, active, onClick }) => {
-  // Determine the icon based on the trend
+const iconMapping = {
+  circleInfo: faCircleInfo,
+  arrowUp: faArrowUp,
+  arrowDown: faArrowDown,
+  gripLines: faGripLines,
+  coffee: faCoffee,
+  anchor: faAnchor,
+  bicycle: faBicycle,
+  bell: faBell,
+  bolt: faBolt,
+  bomb: faBomb,
+  book: faBook,
+  camera: faCamera,
+  car: faCar,
+  appleAlt: faAppleAlt,
+};
+import upTrendImage from '../assets/UP.png'; 
+import midTrendImage from '../assets/MID.png'; 
+import downTrendImage from '../assets/DOWN.png'; 
+import styles from './KpiCard.module.css';
+
+
+const KpiCard = ({ name, tooltipText, icon, value, trend, active, onClick }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const selectedIcon = iconMapping[icon];
+
   const getTrendIcon = (trend) => {
     const trendIcons = {
-      Positive: faArrowUp,
-      Neutral: faGripLines,
-      Negative: faArrowDown,
+      Positive: <img src={upTrendImage} alt="Up Trend" />,
+      Neutral: <img src={midTrendImage} alt="Neutral Trend" />,
+      Negative: <img src={downTrendImage} alt="Down Trend" />,
     };
     return trendIcons[trend] || null;
   };
 
   return (
-    <div className={`kpi-card ${active ? 'active' : ''}`} onClick={onClick}>
-      {/* Conditionally render the tooltip icon if there is a tooltipText */}
-      {tooltipText && (
-        <div className="tooltip-icon">
-          <FontAwesomeIcon icon={faCircleInfo} title={tooltipText} />
+    <div 
+      className={`${styles['kpi-card']} ${active ? styles['active'] : ''}`} 
+      onClick={onClick} 
+      onMouseEnter={() => setIsHovered(true)} 
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {icon && selectedIcon && (
+        <div className={styles['kpi-icon']}>
+          <FontAwesomeIcon icon={selectedIcon} />
         </div>
       )}
-      <div className="kpi-name">{name || 'KPI Name'}</div>
-      <div className="kpi-value">{value || 'KPI Value'}</div>
-      {/* Render trend icon based on the trend value */}
+      <div className={styles['kpi-name']}>{name || 'KPI Name'}</div>
+      <div className={styles['kpi-value']}>{value || 'KPI Value'}</div>
       {trend && (
-        <div className="trend-icon">
-          <FontAwesomeIcon icon={getTrendIcon(trend)} />
+        <div className={styles['trend-icon']}>
+          {getTrendIcon(trend)}
         </div>
+      )}
+      {isHovered && tooltipText && (
+        <div className={styles['tooltip']}>{tooltipText}</div>
       )}
     </div>
-  );
+  );  
 };
 
 export default KpiCard;
--- src/components/KpiConfigPanel.jsx
+++ src/components/KpiConfigPanel.jsx
@@ -1,47 +1,115 @@
-import { useState } from 'react';
-import KpiCard from './KpiCard';
-import KpiConfigPanel from './KpiConfigPanel';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCoffee, faAnchor, faBicycle, faBell, 
+  faBolt, faBomb, faBook, faCamera, 
+  faCar, faAppleAlt
+} from '@fortawesome/free-solid-svg-icons';
 
-const KpiContainer = () => {
-  const [kpis, setKpis] = useState([]);
-  const [activeKpiIndex, setActiveKpiIndex] = useState(null);
+const icons = [
+  faCoffee, faAnchor, faBicycle, faBell, 
+  faBolt, faBomb, faBook, faCamera, 
+  faCar, faAppleAlt
+];
 
-  const addKpi = () => {
-    const newKpi = {
-      name: '',
-      tooltipText: '',
-      icon: '', // Assuming you will use a default icon or empty string for no icon
-      value: '',
-      trend: '', // Could be 'Positive', 'Neutral', or 'Negative'
-    };
-    setKpis([...kpis, newKpi]);
-  };
+const KpiConfigPanel = ({ kpi, onSave }) => {
+  // Local state for form inputs
+  const [name, setName] = useState('');
+  const [tooltipText, setTooltipText] = useState('');
+  const [icon, setIcon] = useState('');
+  const [value, setValue] = useState('');
+  const [trend, setTrend] = useState('');
 
-  const updateKpi = (index, updatedKpi) => {
-    const newKpis = [...kpis];
-    newKpis[index] = updatedKpi;
-    setKpis(newKpis);
+  // Effect to update local state when the kpi prop changes
+  useEffect(() => {
+    setName(kpi.name);
+    setTooltipText(kpi.tooltipText);
+    setIcon(kpi.icon);
+    setValue(kpi.value);
+    setTrend(kpi.trend);
+  }, [kpi]);
+
+  // Function to call onSave with the updated KPI object
+  const handleSave = () => {
+    onSave({
+      ...kpi,
+      name,
+      tooltipText,
+      icon,
+      value,
+      trend
+    });
   };
 
   return (
-    <div>
-      {kpis.map((kpi, index) => (
-        <KpiCard
-          key={index}
-          active={index === activeKpiIndex}
-          {...kpi}
-          onClick={() => setActiveKpiIndex(index)}
+    <div className="kpi-config-panel">
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter KPI name"
         />
-      ))}
-      <button onClick={addKpi}>Add New KPI</button>
-      {activeKpiIndex !== null && (
-        <KpiConfigPanel
-          kpi={kpis[activeKpiIndex]}
-          onSave={(updatedKpi) => updateKpi(activeKpiIndex, updatedKpi)}
+      </label>
+      <label>
+        Tooltip Text:
+        <input
+          type="text"
+          value={tooltipText}
+          onChange={(e) => setTooltipText(e.target.value)}
+          placeholder="Enter tooltip text"
         />
-      )}
+      </label>
+      <label>
+        Icon:
+        <div className="icon-picker">
+          {icons.map((iconObj, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={iconObj}
+              onClick={() => setIcon(iconObj.iconName)}
+              className={`icon-item ${icon === iconObj.iconName ? 'selected' : ''}`}
+            />
+          ))}
+        </div>
+      </label>
+      <label>
+        Value:
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter KPI value"
+        />
+      </label>
+      <label>
+        Trend:
+        <select
+          value={trend}
+          onChange={(e) => setTrend(e.target.value)}
+        >
+          <option value="">Select Trend</option>
+          <option value="Positive">Positive</option>
+          <option value="Neutral">Neutral</option>
+          <option value="Negative">Negative</option>
+        </select>
+      </label>
+      <button onClick={handleSave}>Update KPI</button>
     </div>
   );
 };
 
-export default KpiContainer;
+KpiConfigPanel.propTypes = {
+  kpi: PropTypes.shape({
+    name: PropTypes.string,
+    tooltipText: PropTypes.string,
+    icon: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    trend: PropTypes.string
+  }).isRequired,
+  onSave: PropTypes.func.isRequired
+};
+
+export default KpiConfigPanel;
