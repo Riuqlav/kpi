a6746bb67321905ea1b2f4ccf30fc6315b9a6585
Riuqlav <valquirsolza.vs@gmail.com>
2023-11-09 10:16:23 +0100

    Style card

--- src/components/KpiCard.jsx
+++ src/components/KpiCard.jsx
@@ -1,83 +1,92 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCircleInfo,
-  faArrowUp,
-  faArrowDown,
-  faGripLines,
-  faCoffee,
-  faAnchor,
-  faBicycle,
-  faBell,
-  faBolt,
-  faBomb,
-  faBook,
-  faCamera,
-  faCar,
-  faAppleAlt,
-} from '@fortawesome/free-solid-svg-icons';
+    /* eslint-disable no-unused-vars */
+    /* eslint-disable react/prop-types */
+    import { useState } from 'react';
+    import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+    import {
+      faCircleInfo,
+      faArrowUp,
+      faArrowDown,
+      faGripLines,
+      faCoffee,
+      faAnchor,
+      faBicycle,
+      faBell,
+      faBolt,
+      faBomb,
+      faBook,
+      faCamera,
+      faCar,
+      faAppleAlt,
+    } from '@fortawesome/free-solid-svg-icons';
 
-const iconMapping = {
-  circleInfo: faCircleInfo,
-  arrowUp: faArrowUp,
-  arrowDown: faArrowDown,
-  gripLines: faGripLines,
-  coffee: faCoffee,
-  anchor: faAnchor,
-  bicycle: faBicycle,
-  bell: faBell,
-  bolt: faBolt,
-  bomb: faBomb,
-  book: faBook,
-  camera: faCamera,
-  car: faCar,
-  appleAlt: faAppleAlt,
-};
-import upTrendImage from '../assets/UP.png'; 
-import midTrendImage from '../assets/MID.png'; 
-import downTrendImage from '../assets/DOWN.png'; 
-import styles from './KpiCard.module.css';
-
-
-const KpiCard = ({ name, tooltipText, icon, value, trend, active, onClick }) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const selectedIcon = iconMapping[icon];
-
-  const getTrendIcon = (trend) => {
-    const trendIcons = {
-      Positive: <img src={upTrendImage} alt="Up Trend" />,
-      Neutral: <img src={midTrendImage} alt="Neutral Trend" />,
-      Negative: <img src={downTrendImage} alt="Down Trend" />,
+    const iconMapping = {
+      circleInfo: faCircleInfo,
+      arrowUp: faArrowUp,
+      arrowDown: faArrowDown,
+      gripLines: faGripLines,
+      coffee: faCoffee,
+      anchor: faAnchor,
+      bicycle: faBicycle,
+      bell: faBell,
+      bolt: faBolt,
+      bomb: faBomb,
+      book: faBook,
+      camera: faCamera,
+      car: faCar,
+      appleAlt: faAppleAlt,
     };
-    return trendIcons[trend] || null;
-  };
 
-  return (
-    <div 
-      className={`${styles['kpi-card']} ${active ? styles['active'] : ''}`} 
-      onClick={onClick} 
-      onMouseEnter={() => setIsHovered(true)} 
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      {icon && selectedIcon && (
-        <div className={styles['kpi-icon']}>
-          <FontAwesomeIcon icon={selectedIcon} />
-        </div>
-      )}
-      <div className={styles['kpi-name']}>{name || 'KPI Name'}</div>
-      <div className={styles['kpi-value']}>{value || 'KPI Value'}</div>
-      {trend && (
-        <div className={styles['trend-icon']}>
-          {getTrendIcon(trend)}
-        </div>
-      )}
-      {isHovered && tooltipText && (
-        <div className={styles['tooltip']}>{tooltipText}</div>
-      )}
-    </div>
-  );  
-};
+    import upTrendImage from '../assets/UP.png'; 
+    import midTrendImage from '../assets/MID.png'; 
+    import downTrendImage from '../assets/DOWN.png'; 
+    import styles from './KpiCard.module.css';
 
-export default KpiCard;
+    const KpiCard = ({ name, tooltipText, icon, value, trend, active, onClick }) => {
+      const [isHovered, setIsHovered] = useState(false);
+      const selectedIcon = iconMapping[icon]; 
+
+      const getTrendIcon = (trendValue) => {
+        const trendIcons = {
+          Positive: <img src={upTrendImage} alt="Up Trend" className={styles['trend-image']} />,
+          Neutral: <img src={midTrendImage} alt="Neutral Trend" className={styles['trend-image']} />,
+          Negative: <img src={downTrendImage} alt="Down Trend" className={styles['trend-image']} />,
+        };
+        return trendIcons[trendValue] || null;
+      };
+
+      return (
+        <div className={`${styles['kpi-card']} ${active ? styles['active'] : ''}`} onClick={onClick}>
+          
+          {/* Container for the dynamic icon in the top right */}
+          <div className={styles['icon-top-right']}>
+            {selectedIcon && <FontAwesomeIcon icon={selectedIcon} />}
+          </div>
+    
+          {/* KPI Name with Info Icon */}
+          <div className={styles['kpi-name-container']}>
+            <div className={styles['kpi-name']}>{name || 'KPI Name'}</div>
+            <FontAwesomeIcon 
+              icon={faCircleInfo} 
+              className={styles['info-icon']} 
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            />
+            {isHovered && (
+              <div className={styles['tooltip']}>
+                {tooltipText}
+              </div>
+            )}
+          </div>
+      
+          {/* KPI Value and Trend graph */}
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div className={styles['kpi-value']}>
+              {value || 'KPI Value'}
+            </div>
+            {trend && getTrendIcon(trend)}
+          </div>
+        </div>
+      );
+    };
+    
+    export default KpiCard;
